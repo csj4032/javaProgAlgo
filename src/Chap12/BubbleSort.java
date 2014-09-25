752a3e972c8a0268e96229201ea2a673255220a5
genius <csj4032@gmail.com>
2014-09-25 23:21:49 +0900

    BubbleSort

--- src/Chap12/BubbleSort.java
+++ src/Chap12/BubbleSort.java
@@ -8,7 +8,13 @@ public class BubbleSort {
     public static void sort(int[] a) {
         int n = a.length;
         for(int i = 0; i < n ; i++) {
-
+            for(int j= n -1; j > i; j++) {
+                if(a[j-1] > a[j]) {
+                    int temp = a[j];
+                    a[j] = a[j-1];
+                    a[j-1] = temp;
+                }
+            }
         }
     }
 }
